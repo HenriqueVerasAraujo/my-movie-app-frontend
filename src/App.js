241c7b972daa8f4989556d229e5c9b669f47013d
Henrique_Veras <theveras1@gmail.com>
2022-07-23 01:45:58 -0300

    setup da pagina home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import Home from './pages/Home';
+
 function App() {
   return (
-    <div className="App">
-			<h1 className="text-5xl">teste</h1>
-    </div>
+ <BrowserRouter>
+  <Routes>
+    <Route exact path="/" element={ <Home /> } />
+  </Routes>
+ </BrowserRouter>
   );
 }
-
 export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react'
+
+export default function Home() {
+	const [movieData, setMovieData] = useState([]);
+	const [render, setRender] = useState(false);
+
+	useEffect(() => {
+
+	}, [])
+  return (
+    <div>Home</div>
+  )
+};
