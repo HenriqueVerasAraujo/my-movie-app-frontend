993837c83bce366576cf5b81ebff8335e93edccc
Henrique_Veras <theveras1@gmail.com>
2022-07-23 04:24:23 -0300

    inicio do card de filme

---
+++ src/components/SingleMoviePoster.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function SingleMoviePoster({ movie }) {
+  return (
+    <div className='flex flex-col h-[400px] w-[200px] bg-slate-100 mb-10'>
+        <div className='flex justify-center items-center w-full h-[20%] bg-blue-600 p-3'>
+            <h1 className='text-xl font-bold text-center'>{movie.original_title}</h1>
+        </div>
+        <div>
+            <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt="poster of the movie" />
+        </div>
+    </div>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,32 @@
 import React, { useState, useEffect } from 'react'
+import SingleMoviePoster from '../components/SingleMoviePoster';
+import defaultSearchMovie from '../links/movieFilter';
 
 export default function Home() {
 	const [movieData, setMovieData] = useState([]);
 	const [render, setRender] = useState(false);
 
-	useEffect(() => {
+	const fetchData = async() => {
+		const allData = await fetch(defaultSearchMovie);
+		const allDataJson = await allData.json();
+		console.log(allDataJson.results);
+		setMovieData(allDataJson.results);
+	};
 
-	}, [])
+	useEffect(() => {
+		fetchData();
+		setRender(true);
+	}, []);
+
   return (
-    <div>Home</div>
-  )
+		<div>
+			{render && (
+				movieData.map((s) => (
+					<div key={s.id}>
+						<SingleMoviePoster movie={s} />
+					</div>
+				))
+			)}
+		</div>
+  );
 };
