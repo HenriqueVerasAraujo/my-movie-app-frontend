915296b7526ff579168b816aa757d6392f1d67a4
Henrique_Veras <theveras1@gmail.com>
2022-07-24 01:52:41 -0300

    initial search engine

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,56 @@
 import React, { useState, useEffect } from 'react'
 import SingleMoviePoster from '../components/SingleMoviePoster';
-import defaultSearchMovie from '../links/movieFilter';
+import {defaultSearchMovie, newSearchMovie } from '../links/movieFilter';
 
 export default function Home() {
 	const [movieData, setMovieData] = useState([]);
+	const [inputField, setInputField] = useState('');
 	const [render, setRender] = useState(false);
 
-	const fetchData = async() => {
-		const allData = await fetch(defaultSearchMovie);
+	const fetchData = async(link) => {
+		const allData = await fetch(link);
 		const allDataJson = await allData.json();
 		console.log(allDataJson.results);
 		setMovieData(allDataJson.results);
 	};
 
 	useEffect(() => {
-		fetchData();
+		fetchData(defaultSearchMovie);
 		setRender(true);
 	}, []);
 
+	const formatInputReturn = async() => {
+		const newValue = inputField.split(' ').join('+');
+		await setInputField(newValue);
+	}
+
+	const searchNewMovie = async () => {
+		const newMovieList = `${newSearchMovie}${inputField}`
+		await fetchData(newMovieList)
+	}
+	const searchButton = async() => {
+		if (inputField.length !== 0) {
+			setRender(false);
+			await formatInputReturn();
+			await searchNewMovie()
+			if (movieData.length === 0) {
+				console.log('array vazio');
+			}else {
+				setRender(true);
+			}
+		}
+	};
+
+	const inputFunction = ({ target }) => {
+		setInputField(target.value);
+	};
+
   return (
 		<div>
+			<div>
+				<input type="text" onChange={inputFunction} />
+				<button onClick={searchButton} type='button'>Search</button>
+			</div>
 			{render && (
 				movieData.map((s) => (
 					<div key={s.id}>
