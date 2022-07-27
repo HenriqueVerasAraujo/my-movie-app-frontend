39448037da44ce596912014c33d57cb1bbbd3427
Henrique_Veras <theveras1@gmail.com>
2022-07-27 18:25:52 -0300

    Initial layout of navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,15 +9,23 @@ export default function Navbar() {
   const { movieData, setMovieData } = useContext(myContext);
 
   return (
-    <div className='w-full h-[75px] flex justify-center items-center p-3 bg-sky-800'>
-      <div className='w-full h-full flex items-center bg-blue-700'>
-        <div>
-          <h1 className='text-3xl font-bold text-amber-50'>Logo</h1>
+    <div className='fixed w-full h-[75px] flex justify-center items-center p-3 bg-sky-800'>
+      <div className='w-[95%] h-full flex items-center'>
+
+        <div className='flex items-center w-[20%] h-full'>
+          <h1 className='pl-[40%] text-4xl font-bold text-amber-50'>Logo</h1>
         </div>
+
         <div className='w-[50%] flex'>
           <input className='w-full h-[40px] rounded-l-3xl pl-3' type="text" placeholder='Search a movie by title, actor/actress or genre...' />
           <button className='flex justify-center items-center w-[70px] h-[40px] rounded-r-3xl bg-sky-900 p-3 px-10 text-amber-50 font-bold uppercase' type='button'>Search</button>
         </div>
+
+        <div className='w-[30%] h-full flex items-center justify-evenly'>
+          <h1 className='text-2xl font-bold text-amber-50'>Login</h1>
+          <h1 className='text-2xl font-bold text-amber-50'>Sign Up</h1>
+        </div>
+
       </div>
     </div>
   )
