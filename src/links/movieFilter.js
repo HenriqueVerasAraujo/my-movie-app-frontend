38200cdcccb4701f0866fba46914bae6d632ffa5
Henrique_Veras <theveras1@gmail.com>
2022-08-12 06:12:51 -0300

    https added

--- src/links/movieFilter.js
+++ src/links/movieFilter.js
@@ -8,7 +8,7 @@ export const newSearchMovie = `https://api.themoviedb.org/3/search/movie?include
 export const movieById = (movie_id) => `https://api.themoviedb.org/3/movie/${movie_id}?sort_by=popularity.desc&api_key=${apiKey}&language=en-US`;
 export const movieCastById = (movie_id) => `https://api.themoviedb.org/3/movie/${movie_id}/credits?api_key=${apiKey}&language=en-US`;
 export const findActorName = (actor_name) => `https://api.themoviedb.org/3/search/person?api_key=${apiKey}&language=en-US&query=${actor_name}&page=1&include_adult=false`;
-export const findMovieByActorId = (actor_id) => `http://api.themoviedb.org/3/discover/movie?with_cast=${actor_id}&sort_by=popularity.desc&api_key=${apiKey}&page=1`;
+export const findMovieByActorId = (actor_id) => `https://api.themoviedb.org/3/discover/movie?with_cast=${actor_id}&sort_by=popularity.desc&api_key=${apiKey}&page=1`;
 export const findMovieByGenreId = (genre_id) => `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&page=1&with_genres=${genre_id}`;
 export const trendingMovies = () => `https://api.themoviedb.org/3/trending/movie/week?api_key=${apiKey}`;
 export const popularMovies = () => `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`;
