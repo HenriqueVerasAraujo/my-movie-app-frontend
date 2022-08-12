baffe9666766c640a1c6c1875e90f11c6482018f
Henrique_Veras <theveras1@gmail.com>
2022-08-12 01:04:47 -0300

    Artists with no profile picture now apears with faceless token

--- src/components/MainCastCard.jsx
+++ src/components/MainCastCard.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 import { useNavigate } from 'react-router-dom';
 import myContext from '../context/MyContext';
 import { findMovieByActorId } from '../links/movieFilter';
+import faceless from '../assets/faceless.jpg'
 
 export default function MainCastCard({ info }) {
   const navigate = useNavigate();
@@ -23,7 +24,12 @@ export default function MainCastCard({ info }) {
     <div onClick={redirectFunction} className='flex flex-col justify-center items-center h-[330px]'>
       <div className='flex flex-col items-center justify-center w-[150px] h-[300px] border-neutral-500 border-[1px] rounded-md shadow-[5px_5px_10px_0px_rgba(0,0,0,0.3)] hover:cursor-pointer hover:-translate-y-2 duration-200 ease-in-out'>
           <div className='w-full rounded-md '>
+            {info.profile_path !== null ? (
               <img className='w-full rounded-t-md' src={`https://www.themoviedb.org/t/p/w138_and_h175_face${info.profile_path}`}alt="/" />
+            ) : (
+              <img className='w-full h-[190px] object-cover rounded-t-md' src={ faceless }alt="/" />
+            )}
+            
           </div>
           <div className='h-full w-full flex flex-col items-start p-2 bg-neutral-200 rounded-b-md'>
               <h1 className='font-bold' >{info.name}</h1>
