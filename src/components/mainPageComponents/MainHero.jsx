bd2f6701d0630b6fee1059654ca5f641992c4e70
Henrique_Veras <theveras1@gmail.com>
2022-08-14 23:53:46 -0300

    fix css again

--- src/components/mainPageComponents/MainHero.jsx
+++ src/components/mainPageComponents/MainHero.jsx
@@ -38,8 +38,8 @@ export default function MainHero() {
               </div>
               <div className='w-full h-auto flex flex-col items-center text-slate-100 text-2xl z-10'>
                 <h1 className='font-bold mt-10 text-center'>Search movies, create reviews and rate your favorite films!</h1>
-                <h1 className='flex mt-5'>Don't have an <h1 className='italic mx-2'>Watchables</h1> account yet?</h1>
-                <button onClick={() => {navigate('/register')}}className='mt-10 bg-sky-700 px-3 py-2 text-3xl font-bold border-2 border-sky-600 rounded-md hover:brightness-125'>Click here</button>
+                <h1 className='flex mt-5 text-center'>Don't have an Watchables account yet?</h1>
+                <button onClick={() => {navigate('/register')}}className='mt-7 bg-sky-700 px-3 py-2 text-3xl font-bold border-2 border-sky-600 rounded-md hover:brightness-125'>Click here</button>
               </div>
             </div>
           </div>
