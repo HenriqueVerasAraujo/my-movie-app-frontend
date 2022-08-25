149375c600ad520cc95e3c327804e90cecf7cb09
Henrique_Veras <theveras1@gmail.com>
2022-08-25 04:39:43 -0300

    Fixing genre buttons

--- src/components/heroMovie/GenreButton.jsx
+++ src/components/heroMovie/GenreButton.jsx
@@ -13,7 +13,7 @@ export default function GenreButton({ title, id }) {
 
   return (
       <div onClick={searchButtonGenre}>
-        {title === 'Science Fiction' ? (
+        {title === 'Science Fiction' || title === 'TV Movie' ? (
             <div className='flex items-center justify-center px-4 bg-black opacity-50 w-[140px] h-[40px] rounded-3xl z-0 mr-1 hover:cursor-pointer hover:bg-white hover:opacity-100 hover:border-2 border-black text-white hover:text-black'>
                 <h1 className=' font-bold opacity-200 text-sm z-10'>{title}</h1>
             </div>
